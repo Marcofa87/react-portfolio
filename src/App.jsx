f3726385216b9989713b29f558196d4eaed2d718
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-02 23:03:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+function App() {
+  return (
+    <>
+      <div className="bg-slate-800 h-dvh">
+        <h1 className="text-center text-3xl text-slate-300 font-bold">hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
