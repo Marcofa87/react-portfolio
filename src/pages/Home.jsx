7a0083f95ed49658ee4dc5bd8763e70098a0d3ab
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-03 00:26:28 +0100

    Changed text color of home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-center text-slate-400 font-bold text-3xl">
+      <h1 className="text-center text-3xl from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r bg-clip-text text-transparent">
         Hello! My Name is Marco, and i am a Front-End Developer
       </h1>
     </div>
