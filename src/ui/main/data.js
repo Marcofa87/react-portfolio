45794c6d18b614850ae989b07ad3a8c902272134
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-04 15:43:22 +0100

    Implemented state for changing projects on card

--- src/ui/main/data.js
+++ src/ui/main/data.js
@@ -1,15 +1,32 @@
-export const projectData = {
-  id: 1,
-  image: "../../assets/40005137_10217413760481971_5456129146335789056_n.jpg",
-  title: "Filter-Book-React-app",
-  description:
-    "Book list displayed on a table. You can filter the list by: Title, Author, Release date, Price",
-  sourceCodeLink: {
-    link: "https://github.com/Marcofa87/filter-book-app-react",
-    source: "Source code",
+export const projectData = [
+  {
+    id: 1,
+    image: "../../assets/40005137_10217413760481971_5456129146335789056_n.jpg",
+    title: "Filter-Book-React-app",
+    description:
+      "Book list displayed on a table. You can filter the list by: Title, Author, Release date, Price",
+    sourceCodeLink: {
+      link: "https://github.com/Marcofa87/filter-book-app-react",
+      source: "Source code",
+    },
+    demoLink: {
+      link: "https://book-table-app-react.netlify.app/",
+      demo: "Demo",
+    },
   },
-  demoLink: {
-    link: "https://book-table-app-react.netlify.app/",
-    demo: "Demo",
+  {
+    id: 2,
+    image: "../../assets/40005137_10217413760481971_5456129146335789056_n.jpg",
+    title: "to-do-list",
+    description:
+      "Book list displayed on a table. You can filter the list by: Title, Author, Release date, Price",
+    sourceCodeLink: {
+      link: "https://github.com/Marcofa87/filter-book-app-react",
+      source: "Source code",
+    },
+    demoLink: {
+      link: "https://book-table-app-react.netlify.app/",
+      demo: "Demo",
+    },
   },
-};
+];
