65a261e805248355b101f2fd3f28757eef456ffe
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-11 18:46:31 +0100

    Changed some styling and implemented about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,33 @@
 export default function About() {
   return (
-    <div className="">
-      <p className="text-slate-400">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-        repellendus eos! Delectus, quo asperiores. Quibusdam minus voluptate
-        neque magni, error aliquam. In error est nulla neque, voluptate
-        explicabo reprehenderit et.
+    <>
+      <p className="text-slate-200">
+        Welcome to my corner of the web! I&apos;m a passionate junior front-end
+        developer with hands-on experience in crafting dynamic web applications
+        using React and Angular. With a keen eye for design and a love for
+        clean, efficient code, I thrive on turning creative concepts into
+        seamless user experiences.
       </p>
-    </div>
+
+      <p className="text-slate-200">
+        In my journey as a developer, I&apos;ve honed my skills in HTML, CSS,
+        and JavaScript, always pushing the boundaries of what&apos;s possible in
+        web development. Whether it&apos;s building responsive layouts, or
+        integrating APIs.
+      </p>
+
+      <p className="text-slate-200">
+        Beyond coding, I&apos;m a continual learner, always eager to explore new
+        technologies and methodologies to stay at the forefront of the
+        ever-evolving tech landscape. Collaborative by nature, I thrive in team
+        environments where ideas flow freely and innovation is encouraged.
+      </p>
+
+      <p className="text-slate-200">
+        Explore my portfolio to see some of the projects I&apos;ve brought to
+        life, and feel free to reach out—I&apos;m always up for new challenges
+        and opportunities to contribute to exciting projects.
+      </p>
+    </>
   );
 }
