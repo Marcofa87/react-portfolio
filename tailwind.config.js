1728c2780823e4921ea3638690f8f09aaa9cc343
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-12 20:52:20 +0100

    Imported image in assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "about-section": "url('./assets/new_york.jpeg')",
+      },
+    },
   },
   plugins: [],
 };
