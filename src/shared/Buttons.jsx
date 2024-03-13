329edda3ad03b55977c9ea033bd499ced033cded
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-13 12:48:31 +0100

    implemented some styling at homepage and buttons

--- src/shared/Buttons.jsx
+++ src/shared/Buttons.jsx
@@ -1,3 +1,7 @@
 export default function Buttons({ children }) {
-  return <button className="text-3xl">{children}</button>;
+  return (
+    <button className="text-3xl rounded-2xl  p-2 border-solid border-2 border-slate-400 text-slate-200 font-bold">
+      {children}
+    </button>
+  );
 }
