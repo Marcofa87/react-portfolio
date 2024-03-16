905eb8f04b03a175e34abe233024ada35c1dcfdc
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-16 19:49:22 +0100

    Implemented loading marco falasca

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import Footer from "./ui/footer/Footer";
+import "./index.css";
 
 const About = lazy(() => import("./pages/About"));
 const ProjectsCard = lazy(() => import("./pages/ProjectsCard"));
@@ -10,24 +11,26 @@ const Home = lazy(() => import("./pages/Home"));
 
 function App() {
   return (
-    <div className="bg-slate-800 min-h-screen w-full font-mono flex flex-col">
-      <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          <header>
-            <Navbar />
-          </header>
-          <main className="flex justify-center flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/projects" element={<ProjectsCard />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </main>
-          <Footer className="mt-auto" />
-        </Suspense>
-      </BrowserRouter>
-    </div>
+    <>
+      <div className="bg-slate-800 min-h-screen w-full font-mono flex flex-col">
+        <BrowserRouter>
+          <Suspense fallback={<div className="loader"></div>}>
+            <header>
+              <Navbar />
+            </header>
+            <main className="flex justify-center flex-grow ">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<ProjectsCard />} />
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
+            </main>
+            <Footer className="mt-auto" />
+          </Suspense>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,12 @@
 export default function About() {
   return (
     <>
-      <div
-        className="p-8 border-2 w-10/12 text-center m-0 bg-[url(../../assets/40005137_10217413760481971_5456129146335789056_n.jpg)] bg-cover bg-center"
-        /* style={{ opacity: 0.2 }} */
-      >
-        <div>
+      <div className="relative h-screen w-screen overflow-hidden flex justify-center items-center">
+        <div
+          className="absolute inset-0 h-full w-full p-8 border-2 text-center m-0 bg-[url(../../assets/40005137_10217413760481971_5456129146335789056_n.jpg)] bg-cover bg-center"
+          style={{ opacity: 0.2 }}
+        ></div>
+        <div className="w-5/6 text-center">
           <h1 className="font-bold text-slate-50 text-2xl p-4">
             Something about me
           </h1>
@@ -16,14 +17,12 @@ export default function About() {
             a love for clean, efficient code, I thrive on turning creative
             concepts into seamless user experiences.
           </p>
-
           <p className="text-slate-50">
             In my journey as a developer, I&apos;ve honed my skills in HTML,
             CSS, and JavaScript, always pushing the boundaries of what&apos;s
             possible in web development. Whether it&apos;s building responsive
             layouts, or integrating APIs.
           </p>
-
           <p className="text-slate-50">
             Beyond coding, I&apos;m a continual learner, always eager to explore
             new technologies and methodologies to stay at the forefront of the
@@ -31,7 +30,6 @@ export default function About() {
             team environments where ideas flow freely and innovation is
             encouraged.
           </p>
-
           <p className="text-slate-50">
             Explore my portfolio to see some of the projects I&apos;ve brought
             to life, and feel free to reach out—I&apos;m always up for new
