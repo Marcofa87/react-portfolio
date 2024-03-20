47cbacc19e4a1a7c32535314e6768054f2888977
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-20 18:50:07 +0100

    Implemented Projects and their image

--- src/ui/main/data.js
+++ src/ui/main/data.js
@@ -1,10 +1,10 @@
 export const projectData = [
   {
     id: 1,
-    image: "../../assets/40005137_10217413760481971_5456129146335789056_n.jpg",
-    title: "Filter-Book-React-app",
+    image: "../../../assets//app_pictures/filter-book-app.webp",
+    title: "Filter Book React",
     description:
-      "Book list displayed on a table. You can filter the list by: Title, Author, Release date, Price",
+      "Book list displayed on a table. You can filter the list by: Title, Author, Release date, Price.",
     sourceCodeLink: {
       link: "https://github.com/Marcofa87/filter-book-app-react",
       source: "Source code",
@@ -16,46 +16,101 @@ export const projectData = [
   },
   {
     id: 2,
-    image: "../../assets/40005137_10217413760481971_5456129146335789056_n.jpg",
-    title: "to-do-list",
-    description:
-      "Book list displayed on a table. You can filter the list by: Title, Author, Release date, Price",
+    image: "../../../assets/app_pictures/users_list.webp",
+    title: "Users List ",
+    description: "List of users managed by useState.",
     sourceCodeLink: {
-      link: "https://github.com/Marcofa87/filter-book-app-react",
+      link: "https://github.com/Marcofa87/Users-List",
       source: "Source code",
     },
     demoLink: {
-      link: "https://book-table-app-react.netlify.app/",
+      link: "https://user-lists-react.netlify.app/",
       demo: "Demo",
     },
   },
   {
     id: 3,
-    image: "../../../assets/filter-book-app.avif",
-    title: "Filter-Book-React-app",
-    description:
-      "Book list displayed on a table. You can filter the list by: Title, Author, Release date, Price",
+    image: "../../../assets/app_pictures/my_tip_calulator.webp",
+    title: "Tip Calculator",
+    description: "Layout that displays how much the users have to give for tip",
     sourceCodeLink: {
-      link: "https://github.com/Marcofa87/filter-book-app-react",
+      link: "https://github.com/Marcofa87/tip-calculator",
       source: "Source code",
     },
     demoLink: {
-      link: "https://book-table-app-react.netlify.app/",
+      link: "https://my-personal-tip-calculator.netlify.app/",
       demo: "Demo",
     },
   },
   {
     id: 4,
-    image: "../../assets/40005137_10217413760481971_5456129146335789056_n.jpg",
-    title: "to-do-list",
+    image: "../../../assets/app_pictures/responsive_layer.webp",
+    title: "Responsive Css",
     description:
-      "Book list displayed on a table. You can filter the list by: Title, Author, Release date, Price",
+      "Responsive Webpage that displays for small, medium and large devices",
     sourceCodeLink: {
-      link: "https://github.com/Marcofa87/filter-book-app-react",
+      link: "https://github.com/Marcofa87/ResponsiveWeb",
       source: "Source code",
     },
     demoLink: {
-      link: "https://book-table-app-react.netlify.app/",
+      link: "https://responiveweb.netlify.app/",
+      demo: "Demo",
+    },
+  },
+  {
+    id: 5,
+    image: "../../../assets/app_pictures/calculator_app.webp",
+    title: "Calculator",
+    description: "Calculator made with React",
+    sourceCodeLink: {
+      link: "https://github.com/Marcofa87/react-calculator-app",
+      source: "Source code",
+    },
+    demoLink: {
+      link: "https://react-responsive-calculator-app.netlify.app/",
+      demo: "Demo",
+    },
+  },
+  {
+    id: 6,
+    image: "../../../assets/app_pictures/master_crud_detail.webp",
+    title: "Master Crud Detail",
+    description:
+      "Table of course detail made with Angular with CRUD principles",
+    sourceCodeLink: {
+      link: "https://github.com/Marcofa87/Master-Detail",
+      source: "Source code",
+    },
+    demoLink: {
+      link: "https://master-detail-angular.netlify.app/",
+      demo: "Demo",
+    },
+  },
+  {
+    id: 7,
+    image: "../../../assets/app_pictures/text_expander.webp",
+    title: "Text expander",
+    description: "Text expander made with the same component customized",
+    sourceCodeLink: {
+      link: "https://github.com/Marcofa87/Text-expander",
+      source: "Source code",
+    },
+    demoLink: {
+      link: "https://my-text-expander.netlify.app/",
+      demo: "Demo",
+    },
+  },
+  {
+    id: 8,
+    image: "../../../assets/app_pictures/js_netflix.webp",
+    title: "Netlifix Js",
+    description: "Netlifix clone made with Html, Css and JS (Before ECMA 6).",
+    sourceCodeLink: {
+      link: "https://github.com/Marcofa87/Marcofa87-Netflix-Clone",
+      source: "Source code",
+    },
+    demoLink: {
+      link: "https://js-netflix.netlify.app/",
       demo: "Demo",
     },
   },
