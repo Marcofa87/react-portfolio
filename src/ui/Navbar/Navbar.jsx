835b624f314935953742166708225dfbecd168f4
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-20 19:36:20 +0100

    Changed navbar devide mode

--- src/ui/Navbar/Navbar.jsx
+++ src/ui/Navbar/Navbar.jsx
@@ -13,15 +13,14 @@ export default function Navbar() {
   return (
     <>
       <nav className="flex flex-col md:flex-row items-center justify-between md:border-b-2 p-2">
-        <div className="flex items-center justify-between md:w-2/4">
-          <NavLink
-            to="/"
-            className="justify-start w-1/4 hidden md:block md:w-2/4"
-          >
+        <div className="flex items-center justify-between w-11/12 md:w-2/4">
+          <NavLink to="/" className="justify-start w-2/4  md:block md:w-2/4">
             <NavbarImage />
           </NavLink>
-          <DarkMode />
-          <Language />
+          <div>
+            <DarkMode />
+            <Language />
+          </div>
         </div>
 
         <ul className="md:flex font-bold">
