bc5688f62102851030840130dde6277368be2537
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-22 13:16:16 +0100

    Implementing about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,11 @@
+import thai from "../../public/assets/app_pictures/thai.webp";
 export default function About() {
   return (
     <>
-      <div className="relative h-screen w-screen overflow-hidden flex justify-center p-16">
-        <div className="w-5/6 text-center absolute">
-          <h1 className="font-bold p-4">Something about me</h1>
-          <p>
+      <div className="grid grid-cols-2">
+        <div className="max-w-8/12">
+          <h1 className="font-bold p-4 text-center">Hello!</h1>
+          <p className="p-4">
             Welcome to my corner of the web! I&apos;m a passionate junior
             front-end developer with hands-on experience in crafting dynamic web
             applications using React and Angular, with a good knowledge in the
@@ -14,24 +15,31 @@ export default function About() {
             environments thanks to their teamwork spirit and excellent listening
             and communication skills.
           </p>
-          <p>
+          <p className="p-4">
             In my journey as a developer, I&apos;ve honed my skills in HTML,
             CSS, and JavaScript, always pushing the boundaries of what&apos;s
             possible in web development. Whether it&apos;s building responsive
             layouts, or integrating APIs.
           </p>
-          <p>
+          <p className="p-4">
             Beyond coding, I&apos;m a continual learner, always eager to explore
             new technologies and methodologies to stay at the forefront of the
             ever-evolving tech landscape. Collaborative by nature, I thrive in
             team environments where ideas flow freely and innovation is
             encouraged.
           </p>
-          <p>
+          <p className="p-4">
             Explore my portfolio to see some of the projects I&apos;ve brought
             to life, and feel free to reach out—I&apos;m always up for new
             challenges and opportunities to contribute to exciting projects.
           </p>
+        </div>
+        <div className="max-w-4/12 m-auto">
+          <img
+            className="p-4 opacity-50 rounded-3xl max-h-full"
+            src={thai}
+            alt="picture of me"
+          />
         </div>
       </div>
     </>
