b46927411212863ab21da96301487debd79ae618
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-27 18:09:45 +0100

    Inplemented links component and atyled with my contacts

--- src/ui/main/contacts/Links.jsx
+++ src/ui/main/contacts/Links.jsx
@@ -3,21 +3,32 @@ import { faEnvelope, faPhoneAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Links() {
-  const linkClass = "text-4xl border rounded-full p-4 mr-4";
+  const linkClass = "text-4xl border rounded-full p-4 mb-2 ";
+  const listClass = "m-2 p-4 flex items-center";
   return (
     <ul>
-      <li className="p-4 m-4">
-        <a href="">
-          <FontAwesomeIcon icon={faPhoneAlt} className={linkClass} />
+      <li className={listClass}>
+        <FontAwesomeIcon icon={faPhoneAlt} className={linkClass} />
+        <a href="" className="ml-4">
+          +393472637900
         </a>
-        <a href="">
-          <FontAwesomeIcon icon={faGithub} className={linkClass} />
+      </li>
+      <li className={listClass}>
+        <FontAwesomeIcon icon={faGithub} className={linkClass} />
+        <a href="" className="ml-4">
+          Marcofa87
         </a>
-        <a href="">
-          <FontAwesomeIcon icon={faEnvelope} className={linkClass} />
+      </li>
+      <li className={listClass}>
+        <FontAwesomeIcon icon={faEnvelope} className={linkClass} />
+        <a href="" className="ml-4">
+          marco.falasca87@gmail.com
         </a>
-        <a href="">
-          <FontAwesomeIcon icon={faLinkedin} className={linkClass} />
+      </li>
+      <li className={listClass}>
+        <FontAwesomeIcon icon={faLinkedin} className={linkClass} />
+        <a href="" className="ml-4">
+          marco-falasca-web-developer
         </a>
       </li>
     </ul>
