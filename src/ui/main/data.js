0e3d64a5c2b2d888de772820129856285ac21527
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-27 23:11:40 +0100

    Added dashboard img inside projectcard

--- src/ui/main/data.js
+++ src/ui/main/data.js
@@ -16,7 +16,7 @@ export const projectData = [
   },
   {
     id: 2,
-    image: "./../../../public/assets/app_pictures/dashboard.webp",
+    image: "../../../public/assets/app_pictures/dashboard.webp",
     title: "Dashboard Inventory",
     description: "App for implementing products in a table",
     sourceCodeLink: {
