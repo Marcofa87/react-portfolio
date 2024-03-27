eeb00bdcf9f50e106ac60a732d5cf6651cbeeb40
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-27 16:59:07 +0100

    Styled better with grid projec section

--- src/pages/ProjectsCard.jsx
+++ src/pages/ProjectsCard.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { projectData } from "../ui/main/data";
 import { useTheme } from "../context/ThemeContext";
+import imgBackground from "../../public/assets/code-projects-background.jpg/";
 
 export default function ProjectsCard() {
   const { theme } = useTheme();
+
+  const opacity = theme === "on" ? { opacity: 0.3 } : { opacity: 0.6 };
 
   const imgNotFound = "../../assets/app_pictures/no_imagine.webp";
   const [selectedProjectIndex, setSelectedProjectIndex] = useState(0);
@@ -24,59 +27,59 @@ export default function ProjectsCard() {
 
   return (
     <>
-      <div className="relative min-h-screen w-screen overflow-hidden">
-        <div
-          className="absolute inset-0 h-full w-full bg-[url(../../assets/code-projects-background.jpg)] bg-cover bg-center"
-          style={theme === "on" ? { opacity: 0.4 } : { opacity: 0.9 }}
+      <div className="grid min-h-screen relative w-screen overflow-hidden">
+        <img
+          src={imgBackground}
+          alt="backgorund image of coding"
+          className="absolute h-full w-full"
+          style={opacity}
         />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="w-full max-w-md bg-slate-50 rounded-lg shadow-md p-6 overflow-y-auto">
-            <h2 className="text-center text-2xl text-gray-900 font-bold mb-4">
-              Project {currentProject.id} / {projectData.length}
-            </h2>
-            <img
-              src={!currentProject.image ? imgNotFound : currentProject.image}
-              alt="screen of projects"
-              className="w-11/12 h-60 m-auto rounded-lg mb-4 border-2 border-amber-700"
-            />
-            <p className="text-slate-900 font-bold text-2xl mb-2 text-center">
-              {currentProject.title}
-            </p>
-            <p className="text-gray-900 text-sm mb-4 font-bold">
-              {currentProject.description}
-            </p>
-            <div className="flex justify-between mb-4">
-              <a
-                href={currentProject.sourceCodeLink.link}
-                className="text-blue-700 hover:underline font-bold"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {currentProject.sourceCodeLink.source}
-              </a>
-              <a
-                href={currentProject.demoLink.link}
-                className="text-blue-700 hover:underline font-bold"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {currentProject.demoLink.demo}
-              </a>
-            </div>
-            <div className="flex justify-between">
-              <button
-                className="px-4 py-2 rounded-md bg-slate-200 text-gray-800 hover:bg-gray-300 focus:outline-none"
-                onClick={handlePrevIndex}
-              >
-                Back
-              </button>
-              <button
-                className="px-4 py-2 rounded-lg bg-slate-200 text-gray-800 hover:bg-gray-300 focus:outline-none"
-                onClick={handleNextIndex}
-              >
-                Next
-              </button>
-            </div>
+        <div className="w-full m-auto items-center max-w-md bg-slate-50 rounded-lg shadow-md p-6 overflow-y-auto z-20">
+          <h2 className="text-center text-2xl text-gray-900 font-bold mb-4">
+            Project {currentProject.id} / {projectData.length}
+          </h2>
+          <img
+            src={!currentProject.image ? imgNotFound : currentProject.image}
+            alt="screen of projects"
+            className="w-11/12 h-60 m-auto rounded-lg mb-4 border-2 border-amber-700"
+          />
+          <p className="text-slate-900 font-bold text-2xl mb-2 text-center">
+            {currentProject.title}
+          </p>
+          <p className="text-gray-900 text-sm mb-4 font-bold">
+            {currentProject.description}
+          </p>
+          <div className="flex justify-between mb-4">
+            <a
+              href={currentProject.sourceCodeLink.link}
+              className="text-blue-700 hover:underline font-bold"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {currentProject.sourceCodeLink.source}
+            </a>
+            <a
+              href={currentProject.demoLink.link}
+              className="text-blue-700 hover:underline font-bold"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {currentProject.demoLink.demo}
+            </a>
+          </div>
+          <div className="flex justify-between">
+            <button
+              className="px-4 py-2 rounded-md bg-slate-200 text-gray-800 hover:bg-gray-300 focus:outline-none"
+              onClick={handlePrevIndex}
+            >
+              Back
+            </button>
+            <button
+              className="px-4 py-2 rounded-lg bg-slate-200 text-gray-800 hover:bg-gray-300 focus:outline-none"
+              onClick={handleNextIndex}
+            >
+              Next
+            </button>
           </div>
         </div>
       </div>
