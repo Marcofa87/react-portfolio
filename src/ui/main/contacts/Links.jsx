238f0d3f0a63029bc4535fe231608304303a32ae
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-28 12:51:28 +0100

    Refactor of Links component

--- src/ui/main/contacts/Links.jsx
+++ src/ui/main/contacts/Links.jsx
@@ -10,40 +10,40 @@ import { SiGmail } from "react-icons/si";
 export default function Links() {
   const linkClass = "text-4xl border rounded-3xl p-4 mb-2 ";
   const listClass = "m-2 p-4 flex items-center";
+
+  const contactLinks = [
+    {
+      icon: <FontAwesomeIcon icon={faWhatsapp} className={linkClass} />,
+      href: "https://wa.me/+393472637900",
+      text: "Send me a message",
+    },
+    {
+      icon: <FontAwesomeIcon icon={faGithub} className={linkClass} />,
+      href: "https://github.com/Marcofa87",
+      text: "Github",
+    },
+    {
+      icon: <SiGmail className={`text-7xl ${linkClass}`} />,
+      href: "mailto:marco.falasca87@gmail.com",
+      text: "Email: marco.falasca87@gmail.com",
+    },
+    {
+      icon: <FontAwesomeIcon icon={faLinkedin} className={linkClass} />,
+      href: "https://www.linkedin.com/in/marco-falasca-web-developer/",
+      text: "Linkedin",
+    },
+  ];
+
   return (
     <ul>
-      <li className={listClass}>
-        <FontAwesomeIcon icon={faWhatsapp} className={linkClass} />
-        <a href="https://wa.me/+393472637900" target="_blank" className="ml-4">
-          Send me a message
-        </a>
-      </li>
-      <li className={listClass}>
-        <FontAwesomeIcon icon={faGithub} className={linkClass} />
-        <a href="https://github.com/Marcofa87" target="_blank" className="ml-4">
-          Github
-        </a>
-      </li>
-      <li className={listClass}>
-        <SiGmail className={`text-7xl ${linkClass}`} />
-        <a
-          href="mailto:marco.falasca87@gmail.com"
-          target="_blank"
-          className="ml-4"
-        >
-          Email: marco.falasca87@gmail.com
-        </a>
-      </li>
-      <li className={listClass}>
-        <FontAwesomeIcon icon={faLinkedin} className={linkClass} />
-        <a
-          href="https://www.linkedin.com/in/marco-falasca-web-developer/"
-          target="_blank"
-          className="ml-4"
-        >
-          Linkedin
-        </a>
-      </li>
+      {contactLinks.map((link, id) => (
+        <li key={id} className={listClass}>
+          {link.icon}
+          <a href={link.href} target="_blank" className="ml-4">
+            {link.text}
+          </a>
+        </li>
+      ))}
     </ul>
   );
 }
---
+++ src/ui/main/contacts/contactLinks.js
@@ -0,0 +1,32 @@
+import {
+  faGithub,
+  faLinkedin,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { SiGmail } from "react-icons/si";
+
+const linkClass = "text-4xl border rounded-3xl p-4 mb-2 ";
+
+export const contactLinks = [
+  {
+    icon: <FontAwesomeIcon icon={faWhatsapp} className={linkClass} />,
+    href: "https://wa.me/+393472637900",
+    text: "Send me a message",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faGithub} className={linkClass} />,
+    href: "https://github.com/Marcofa87",
+    text: "Github",
+  },
+  {
+    icon: <SiGmail className={`text-7xl ${linkClass}`} />,
+    href: "mailto:marco.falasca87@gmail.com",
+    text: "Email: marco.falasca87@gmail.com",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faLinkedin} className={linkClass} />,
+    href: "https://www.linkedin.com/in/marco-falasca-web-developer/",
+    text: "Linkedin",
+  },
+];
