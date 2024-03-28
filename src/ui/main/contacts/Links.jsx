da849c79ad0f768b3d76cbc7ad3f3f1a62ae1b4a
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-03-28 12:29:36 +0100

    Changed email icon

--- src/ui/main/contacts/Links.jsx
+++ src/ui/main/contacts/Links.jsx
@@ -3,12 +3,12 @@ import {
   faLinkedin,
   faWhatsapp,
 } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { SiGmail } from "react-icons/si";
 
 export default function Links() {
-  const linkClass = "text-4xl border rounded-full p-4 mb-2 ";
+  const linkClass = "text-4xl border rounded-3xl p-4 mb-2 ";
   const listClass = "m-2 p-4 flex items-center";
   return (
     <ul>
@@ -25,7 +25,7 @@ export default function Links() {
         </a>
       </li>
       <li className={listClass}>
-        <SiGmail className={linkClass} />
+        <SiGmail className={`text-7xl ${linkClass}`} />
         <a
           href="mailto:marco.falasca87@gmail.com"
           target="_blank"
